9fe1f634e91c49c373da45ebf8e8ab0a518d0e9d
r-i-s-u <114793497+r-i-s-u@users.noreply.github.com>
2026-06-15 20:55:03 +0530

    Update LoadingDialog.java

--- app/src/main/java/org/schabi/newpipe/download/LoadingDialog.java
+++ app/src/main/java/org/schabi/newpipe/download/LoadingDialog.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.graphics.drawable.ColorDrawable;
+import android.view.Window;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;

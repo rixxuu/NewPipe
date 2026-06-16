19b80ca567dca8e3a065fe6ce2020f4e4b4e2d03
r-i-s-u <114793497+r-i-s-u@users.noreply.github.com>
2026-06-16 14:24:01 +0530

    Update DownloadDialog.java

--- app/src/main/java/org/schabi/newpipe/download/DownloadDialog.java
+++ app/src/main/java/org/schabi/newpipe/download/DownloadDialog.java
@@ -196,7 +196,7 @@ public class DownloadDialog extends DialogFragment
     @Override
     public void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-            setStyle(STYLE_NO_TITLE, com.google.android.material.R.style.ThemeOverlay_Material3_Dialog);
+            setStyle(STYLE_NO_TITLE, com.google.android.material.R.style.Theme.Material3.DayNight.Dialog);
         if (DEBUG) {
             Log.d(TAG, "onCreate() called with: "
                     + "savedInstanceState = [" + savedInstanceState + "]");

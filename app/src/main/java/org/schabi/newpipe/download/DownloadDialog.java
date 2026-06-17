4a123c20e8affd6f46575c72ffd993e3dd64f650
r-i-s-u <114793497+r-i-s-u@users.noreply.github.com>
2026-06-17 20:50:51 +0530

    Update DownloadDialog.java

--- app/src/main/java/org/schabi/newpipe/download/DownloadDialog.java
+++ app/src/main/java/org/schabi/newpipe/download/DownloadDialog.java
@@ -16,7 +16,7 @@ import android.os.IBinder;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
+import androidx.appcompat.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
